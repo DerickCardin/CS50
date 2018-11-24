0e6d0a708857228d573ca4e2c94ab9e73d11c8a8
Derick Cardin <derickc50@gmail.com>
2018-11-24 22:37:45 +0000

    Completed Cash Solution

--- PSet-1/cash.c
+++ PSet-1/cash.c
@@ -1,12 +1,48 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    int change;
+    float change;
+    int cents;
+    int quarters;
+    int dimes;
+    int nickels;
+    int pennies;
+    int totalcoins = 0;
+
 do
 {
-  change = get_int("Change: ");
+  change = get_float("Change: ");
 }
-while ( change < 0);
+while ( change < 0.0);
+
+cents = round(change * 100.00);
+
+while (cents >= 25)
+{
+  quarters = cents/25;
+  totalcoins += quarters;
+  cents -=(quarters * 25);
+}
+while (cents >= 10)
+{
+    dimes = cents/10;
+    totalcoins += dimes;
+    cents -= (dimes * 10);
+}
+while (cents >= 5)
+{
+    nickels = cents/5;
+    totalcoins += nickels;
+    cents -= (nickels * 5);
+}
+while (cents >= 1)
+{
+    pennies = cents/1;
+    totalcoins += pennies;
+    cents -= (pennies * 1);
+}
+printf("%i\n", totalcoins);
 }
