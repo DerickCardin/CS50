2ced4fe39bc56f5836be66c8ad32017e295e5a65
Derick Cardin <derickc50@gmail.com>
2018-11-24 21:19:19 +0000

    Updated line breaks and validation

--- PSet-1/mario.c
+++ PSet-1/mario.c
@@ -1,22 +1,32 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
     int height;
-do
-{
-    height = get_int("Height: ");
+    int space;
+    int hash;
+    int row;
 
-}
-while (height <= 0 && height >= 23);
-for (int space = height -1; space <= 23; space++)
-{
-    printf(" ");
-}
-for (int hash = 0; hash < height; hash++)
-{
-    printf("#");
+    do
+    {
+        height = get_int("Height: ");
+    }
 
-}
+    while (height < 0 || height > 23);
+
+    for (row = 0; row < height; row++)
+    {
+  for (space = 1; space <= height - 1 - row; space++)
+        {
+            printf(" ");
+        }
+
+        for (hash = 0; hash < row + 1; hash++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
 }
