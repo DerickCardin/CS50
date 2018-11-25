bc5a8a21f3bec439dead7339411d7b131a1788c5
Derick Cardin <derickc50@gmail.com>
2018-11-25 00:15:16 +0000

    Updated water style

--- PSet-1/water/water.c
+++ PSet-1/water/water.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    int minutes = get_int("Minutes: ");
-    printf("Bottles: %i\n", minutes * 12);
+    int minutes = get_int("Minutes: "); //Get Minutes from user
+    printf("Bottles: %i\n", minutes * 12); //Take minutes and multiply by 12(bottles of water per minute)
 }
