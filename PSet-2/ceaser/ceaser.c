8a111f2da326d0a41778250b8e9ee42269467325
Derick Cardin <derickc50@gmail.com>
2018-11-25 22:22:21 +0000

    PSet-2 Psuedocode for ceaser

---
+++ PSet-2/ceaser/ceaser.c
@@ -0,0 +1,9 @@
+//PSUEDO-CODE
+//Get key from command line argument
+//turn key into integer
+//prompt for plain text
+// for each character in the plain text string
+//if alphabetic
+//preserve case
+//shift plain text by key
+//print cypher text
