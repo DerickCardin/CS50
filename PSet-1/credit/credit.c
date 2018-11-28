00d6b7d13ee8e2c28b950d28d44e8d69e8c5818a
Derick Cardin <derickc50@gmail.com>
2018-11-28 00:41:56 +0000

    Added cc length validator

--- PSet-1/credit/credit.c
+++ PSet-1/credit/credit.c
@@ -1,8 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+#define MAX16 9999999999999999
+#define MIN16 1000000000000000
+
 int main(void)
 {
-
-    printf("Hello, world\n");
+    long long ccnum = get_long_long("Number: ");
+    printf("%lld\n", ccnum);
+    if (ccnum >= MIN16 && ccnum <= MAX16)
 }
+
+//378282246310005
