c98ad1a76198af9a185fa6126bde6a8c25644781
Derick Cardin <derickc50@gmail.com>
2018-12-07 00:16:23 +0000

    Updated helpers.c with notes

--- PSet-3/music/helpers.c
+++ PSet-3/music/helpers.c
@@ -13,11 +13,11 @@ int duration(string fraction)
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    // TODO
+    // TODO return an int in beats and use wav.length to convert beats to actual time. set beat to 1/8 and have a fraction coverter for 1/2 and 1/4
 }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    // TODO
+    // TODO check if there is a line break or .\n or .\r and if so return true if not return false
 }
