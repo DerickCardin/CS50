5be975c1b6ccb1159827dd485e88a8d5a79cd85e
Derick Cardin <derickc50@gmail.com>
2018-12-08 17:20:37 +0000

    Updated test3 file for string comparison

--- PSet-3/test3.c
+++ PSet-3/test3.c
@@ -1,23 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 bool is_rest(string);
+int check;
 
 int main(void)
 {
-char* s = "No line break";
-
+string str1 = "No line break\n";
+string str2 = "\n";
+check = strcmp(str1, str2);
 }
-bool is_rest(string s)
+bool is_rest(string str1)
 {
-   if (s[0] == 0)
-   {
-       return true;
-       printf("true");
-   }
-   else
-   {
-       return false;
-       printf("False");
-   }// TODO check if there is a line break or .\n or .\r and if so return true if not return false
+  if (check == 0)
+  {
+      printf("True");
+      return true;
+
+  }
+  else
+  {
+
+      printf("False");
+      return false;
+  }// TODO check if there is a line break or .\n or .\r and if so return true if not return false
 }
